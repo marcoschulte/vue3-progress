67c6cc1bdd198cced209eae05be805442a098ff3
Marco Schulte <marco.schulte@eckert-partner.it>
2022-08-06 09:56:52 +0200

    Export trickleComposable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import {useProgress} from './Injector';
 import ProgressBar from './ProgressBar.vue';
 import {ProgressControls, ProgressFinisher} from './ProgressControls';
 import {ProgressOptions, Vue3ProgressPlugin} from './ProgressPlugin';
-import {useProgress} from './Injector';
+import trickleComposable from './TrickleComposable';
 
-export {Vue3ProgressPlugin, useProgress, ProgressOptions, ProgressControls, ProgressFinisher, ProgressBar};
+export {Vue3ProgressPlugin, useProgress, ProgressOptions, ProgressControls, ProgressFinisher, ProgressBar, trickleComposable};
